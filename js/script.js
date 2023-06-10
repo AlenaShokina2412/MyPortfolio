fda97f659e8ac1fd16a73d71faaf8e3759edc325
AlenaShokina2412 <112576495+AlenaShokina2412@users.noreply.github.com>
2023-06-10 16:43:52 +0300

    выполнила 1 часть задания 9)

---
+++ js/script.js
@@ -0,0 +1,65 @@
+const skill = {
+    data : [
+        {
+            item: "html",
+            level: 50,
+            iconPath: "img/html.svg",
+        },
+        {
+            item: "css",
+            level: 50,
+            iconPath: "img/css.svg",
+        },
+        {
+            item: "figma",
+            level: 50,
+            iconPath: "img/figma.svg",
+        },
+        {
+            item: "C++",
+            level: 70,
+            iconPath: "img/c++.svg",
+        },
+        {
+            item: "java",
+            level: 70,
+            iconPath: "img/java.svg",
+        },
+        {
+            item: "1С",
+            level: 100,
+            iconPath: "img/1c.svg",
+        }
+    ],
+
+    generateList : function(parentElement) {
+        this.data.forEach(element => {
+            const Dta = document.createElement('dt');
+            const Dda = document.createElement('dd');
+            const Diva = document.createElement('div');
+        
+            Dta.classList.add('skill-item');
+            Dda.classList.add('skill-level');
+        
+        
+            Dta.textContent = element.item;
+            Diva.textContent = `${element.level}%`;
+        
+            Diva.style.width = `${element.level}%`;
+        
+            Dta.style.backgroundImage = `url(${element.iconPath})`;
+        
+            
+        
+            parentElement.appendChild(Dta);
+            parentElement.appendChild(Dda);
+        
+            Dda.appendChild(Diva);
+            
+        });
+    }
+};
+skill.generateList(document.querySelector("dl.skill-list"));
+
+
+
